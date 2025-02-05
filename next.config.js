1630c011382c9d7ca7fce71e778c02206f3ed3db
Igor24011999 <154072452+Igor24011999@users.noreply.github.com>
2025-02-05 20:02:23 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/deepseek-chat',
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
